7462f38e5a327d098190618eb29b1259f93ec17f
wangpeng16@100tal.com <wangpeng16@100tal.com>
2020-06-17 17:20:56 +0800

    feat: 两数相加

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -18,7 +18,30 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+  const pre = new ListNode(0);
+  let cur = pre;
+  let carry = 0;
+  while(l1 || l2) {
+    let x = !l1 ? 0 : l1.val;
+    let y = !l2 ? 0 : l2.val;
+    let sum = x + y + carry;
 
+    carry = sum >9 ? 1 : 0 ;;
+    sum = sum % 10;
+    cur.next = new ListNode(sum);
+
+    cur = cur.next;
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  if (carry === 1) {
+    cur.next = new ListNode(carry);
+  }
+  return pre.next;
 };
 // @lc code=end
 
